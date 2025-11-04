0135eeabf2c5774fa399a5f3a2168c7c83fbc3c7
wendel211 <wendelmuniz04@gmail.com>
2025-11-03 22:57:25 -0300

    feat: Create DTO for user creation with validations.

---
+++ src/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @MinLength(6)
+  password: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto, UpdateUserDto } from './dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private repo: Repository<User>,
+  ) {}
+
+  create(data: CreateUserDto) {
+    const user = this.repo.create(data);
+    return this.repo.save(user);
+  }
+
+  findAll() {
+    return this.repo.find({ relations: ['profiles'] });
+  }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOne({ where: { id }, relations: ['profiles'] });
+    if (!user) throw new NotFoundException(`Usuário ${id} não encontrado`);
+    return user;
+  }
+
+  async update(id: number, data: UpdateUserDto) {
+    const user = await this.findOne(id);
+    Object.assign(user, data);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    return this.repo.remove(user);
+  }
+}

0ab92d0205b0af222c8ea954228b36beb290c610
wendel211 <wendelmuniz04@gmail.com>
2025-11-04 14:26:07 -0300

    feat: updating NestJS dependencies and fixing DTO import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ProfilesModule } from './profiles/profiles.module';
-import { ProfilesModule } from './profiles/profiles.module';
+
 
 @Module({
   imports: [
